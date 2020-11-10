f3e5712199e3d731676d83dae6496649268d39d8
Furkan Kamer <furkan.kamer@brn.local>
2020-11-10 10:29:54 +0300

    button styling

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -2,6 +2,9 @@
 #include "ui_mainwindow.h"
 #include <iostream>
 #include<string>
+#include <QSqlDatabase>
+#include <QtSql>
+
 int onclick = 0;
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
@@ -15,9 +18,20 @@ MainWindow::~MainWindow()
     delete ui;
 }
 
-
+QString GetDataFromDataBase(QString query){
+    QSqlDatabase db = QSqlDatabase::addDatabase("QPSQL");
+    db.setHostName("hattie.db.elephantsql.com");
+    db.setDatabaseName("lvzhcnac");
+    db.setUserName("lvzhcnac");
+    db.setPassword("FjnjB28yNrnKOwp_coyq7LABdtIL2iIK");
+    bool ok = db.open();
+    QString result = "success";
+    if(!db.open())
+         result = "Failed";
+    return result;
+}
 
 void MainWindow::on_pushButton_clicked()
 {
-    ui->pushButton->setText(QString::number(onclick++));
+    ui->pushButton->setText(GetDataFromDataBase("asd"));
 }
